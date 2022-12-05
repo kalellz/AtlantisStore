8aed6371e8ff1a7fe2a9095a8ce7f7d9f21e2e93
Kalel <ZoroPlay1220@gmail.com>
2022-12-05 18:54:17 -0300

    update error title

--- components/productsHigh.js
+++ components/productsHigh.js
@@ -6,8 +6,8 @@ export default function Products(props){
             <div className={styles.image}>
                 <img src={props.image} />
             </div>
-            <div>
-                <h1 style={{fontSize:"1em"}}>{props.title}</h1>
+            <div >
+                <h1>{props.title}</h1>
             </div>
             <div>
                 <h2>Por <span style={{ color: "#ED0842" }}>R${props.price},00</span></h2>
