50d2e01d4b6d0a786e182bd047635e958874c969
Kalel <ZoroPlay1220@gmail.com>
2022-12-05 18:56:58 -0300

    update error size font

--- components/productsHigh.js
+++ components/productsHigh.js
@@ -6,14 +6,14 @@ export default function Products(props){
             <div className={styles.image}>
                 <img src={props.image} />
             </div>
-            <div >
-                <h1>{props.title}</h1>
+            <div>
+                <h2>{props.title}</h2>
             </div>
             <div>
-                <h2>Por <span style={{ color: "#ED0842" }}>R${props.price},00</span></h2>
+                <h3>Por <span style={{ color: "#ED0842" }}>R${props.price},00</span></h3>
             </div>
             <div>
-                <h3>ou {props.parcel}</h3>
+                <h4>ou {props.parcel}</h4>
             </div>
             <button>Abrir Anuncio</button>
         </div>
