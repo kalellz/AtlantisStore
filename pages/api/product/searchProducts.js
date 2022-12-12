5706f9856bd4df240dda7875e5d883879b525f0a
Kalel <ZoroPlay1220@gmail.com>
2022-12-12 15:00:46 -0300

    getFUNC!!!

--- pages/api/product/searchProducts.js
+++ pages/api/product/searchProducts.js
@@ -4,7 +4,7 @@ export default async function search(req, res) {
     try {
         const db = await connectToDatabase(process.env.MONGODB_URI)
         const collection = db.collection('products')
-        const products = await collection.find({})
+        const products = await collection.find({}).toArray()
         return res.status(200).send(products)
     } catch (err) {
         return res.status(401).send({
